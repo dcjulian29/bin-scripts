1a6bfd32e39f34e0396673ed344c995a87321cd0
Julian Easterling <julian@julianscorner.com>
2012-08-25 15:32:38 -0400

    Removed TERM environment variable and added symbol source environment variables.

--- scripts/Set-Envronment-Variables.cmd
+++ scripts/Set-Envronment-Variables.cmd
@@ -1,5 +1,8 @@
 @echo off
 
-REG ADD "HKCU\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v HOME /d %%USERPROFILE%%
-REG ADD "HKCU\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v TERM /d cygwin
+REG ADD "HKCU\Environment" /v HOME /d %%USERPROFILE%%
 
+:: Need to be elevated...
+setx /m _NT_SOURCE_PATH SRV*C:\Temp
+setx /m _NT_SYMBOL_PATH SRV*C:\Temp*http://msdl.microsoft.com/download/symbols
+setx /m _NT_SYMCACHE_PATH C:\Temp
