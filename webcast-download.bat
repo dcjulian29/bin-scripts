5cfec9b264ddbd1c922f76c983ebb42bb591e0bf
Julian Easterling <julian@julianscorner.com>
2012-09-02 05:05:22 -0400

    Added ability to specify directory for downloaded webcast

--- webcast-download.bat
+++ webcast-download.bat
@@ -1,3 +1,28 @@
 @echo off
 
-%SystemDrive%\bin\network\wget\wget.exe %1 -P "D:\Videos\_download" --continue --tries=10 --restrict-file-names=windows
+setlocal
+
+if [%1] == [] goto PROVIDESRC
+
+set SRC=%1
+
+set DST=D:\Videos\_download
+
+if [%2] == [] goto STARTDOWNLOAD
+
+set DST=%2
+
+:STARTDOWNLOAD
+
+%SystemDrive%\bin\network\wget\wget.exe %SRC% -P "%dst%" --continue --tries=10 --restrict-file-names=windows
+
+goto EOF
+
+
+:PROVIDESRC
+
+echo.
+echo Please provide the URL to download from...
+echo.
+
+:EOF
