5fd0c0dcfc64c110911fbda7236e605073848c8b
Julian Easterling <julian@julianscorner.com>
2012-11-15 13:18:24 -0500

    scripts to set up my symbolic links.

---
+++ scripts/make-my-directory-links-tempdirs.cmd
@@ -0,0 +1,61 @@
+@echo off
+
+echo.
+echo Don't run this script directly...
+echo.
+echo 1. Log in with an alternative account (elevate?)
+echo 2. Open the script in Notepad
+echo 3. Run commands in an elevated shell as needed...
+echo.
+goto EOF
+
+
+::------------------------------------------------------------------------------
+
+setlocal
+set UP=%SYSTEMDRIVE%\Users\jeast836
+
+::------------------------------------------------------------------------------
+:: Temporary "Stuff" Directories
+::------------------------------------------------------------------------------
+
+SET T=%SYSTEMDRIVE%\Temp
+
+rmdir /s /q  %T% >nul
+:: Base Temporary Directory (point to RAM Drive if available)
+mklink /D %T% E:\
+:: If no RAM drive, set system temporary directory to %SYSTEMDRIVE%\Temp
+mkdir %SYSTEMDRIVE%\Temp
+
+::------------------------------------------------------------------------------
+
+rmdir /s /q "%UP%\AppData\Local\Temp" >nul
+mklink /D "%UP%\AppData\Local\Temp" %T%
+rmdir /s /q "%SYSTEMROOT%\Temp" >nul
+mklink /D "%SYSTEMROOT%\Temp" %T%
+
+:: 32-Bit OS .Net Frameworks
+rmdir /s /q "%SYSTEMROOT%\Microsoft.NET\Framework\v2.0.50727\Temporary ASP.NET Files" 2>nul
+mklink /D "%SYSTEMROOT%\Microsoft.NET\Framework\v2.0.50727\Temporary ASP.NET Files" %T%
+rmdir /s /q "%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\Temporary ASP.NET Files" 2>nul
+mklink /D "%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\Temporary ASP.NET Files" %T%
+
+:: 64-Bit OS .Net Frameworks
+rmdir /s /q "%SYSTEMROOT%\Microsoft.NET\Framework64\v2.0.50727\Temporary ASP.NET Files" 2>nul
+mklink /D "%SYSTEMROOT%\Microsoft.NET\Framework64\v2.0.50727\Temporary ASP.NET Files" %T%
+rmdir /s /q "%SYSTEMROOT%\Microsoft.NET\Framework64\v4.0.30319\Temporary ASP.NET Files" 2>nul
+mklink /D "%SYSTEMROOT%\Microsoft.NET\Framework64\v4.0.30319\Temporary ASP.NET Files" %T%
+
+:: Browser Cache - IE
+rmdir /s /q "%UP%\AppData\Local\Microsoft\Windows\Temporary Internet Files" 2>nul
+mklink /D "%UP%\AppData\Local\Microsoft\Windows\Temporary Internet Files" %T%
+
+:: Browser Cache - Chrome
+rmdir /s /q "%UP%\AppData\Local\Google\Chrome\User Data\Temp" 2>nul
+mklink /D "%UP%\AppData\Local\Google\Chrome\User Data\Temp" %T%
+
+:: Browser Cache - Firefox
+rmdir /s /q "%UP%\AppData\Local\Mozilla\Firefox\Profiles\vj6gmdj0.default\Cache" 2>nul
+mklink /D "%UP%\AppData\Local\Mozilla\Firefox\Profiles\vj6gmdj0.default\Cache" %T%
+
+
---
+++ scripts/make-my-directory-links.cmd
@@ -0,0 +1,63 @@
+@echo off
+
+echo.
+echo Don't run this script directly...
+echo.
+echo 1. Log in with an alternative account (elevate?)
+echo 2. Open the script in Notepad
+echo 3. Run commands in an elevated shell as needed...
+echo.
+goto EOF
+
+
+::------------------------------------------------------------------------------
+
+setlocal
+set UP=%SYSTEMDRIVE%\Users\jeast836
+
+::------------------------------------------------------------------------------
+
+:: PowerShell
+rmdir /s /q %UP%\Documents\WindowsPowerShell >nul
+mklink /J %UP%\Documents\WindowsPowerShell %SYSTEMDRIVE%\bin\WindowsPowerShell
+
+:: SSH
+mklink /J %UP%\.ssh %SYSTEMDRIVE%\etc\ssh
+
+:: GIT
+mklink %UP%\.gitconfig %SYSTEMDRIVE%\etc\git\gitconfig
+mklink %UP%\.gitattributes %SYSTEMDRIVE%\etc\git\gitattributes
+mklink %UP%\.gitignore %SYSTEMDRIVE%\etc\git\gitignore
+mklink %UP%\.gitk %SYSTEMDRIVE%\etc\git\gitk
+
+:: SVN
+mklink /J %UP%\.subversion %SYSTEMDRIVE%\etc\subversion
+mklink /J %UP%\AppData\Roaming\Subversion %SYSTEMDRIVE%\etc\subversion
+
+:: Mecurial
+mklink %UP%\.hgrc %SYSTEMDRIVE%\etc\mecurial\hgrc
+
+:: Cygwin
+mklink %UP%\.bash_profile %SYSTEMDRIVE%\etc\cygwin\bash_profile
+mklink %UP%\.bashrc %SYSTEMDRIVE%\etc\cygwin\bashrc
+mklink %UP%\.inputrc %SYSTEMDRIVE%\etc\cygwin\inputrc
+mklink %UP%\.minttyrc %SYSTEMDRIVE%\etc\cygwin\minttyrc
+mklink %UP%\.Xresources %SYSTEMDRIVE%\etc\cygwin\Xresources
+
+:: Downloads, Pictures, Videos
+rmdir /s /q %UP%\Downloads >nul
+mklink /D %UP%\Downloads D:\Downloads
+rmdir /s /q %UP%\Pictures >nul
+mklink /D %UP%\Pictures D:\jnet\pictures
+rmdir /s /q %UP%\Videos >nul
+mklink /D %UP%\Videos D:\jnet\videos
+
+:: Document Folder Links
+mklink /D "%UP%\Documents\JNet" D:\JNet\docs
+mklink /D "%UP%\Documents\JCoG" D:\JCoG\docs
+mklink /D "%UP%\Documents\Marriott" D:\Marriott\docs
+mklink /D "%UP%\Documents\eBooks" D:\eBooks
+
+:: Virtual Machines
+mklink /D "%UP%\Documents\Virtual Machines" D:\VM
+
