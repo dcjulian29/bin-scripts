b76a0732dba66bbfd9285cf406fdd5c4db90a2ba
Julian Easterling <julian@julianscorner.com>
2012-11-15 12:59:40 -0500

    Removed directory since I don't use the scripts anymore.

--- midesktop/GoMI.bat
+++
@@ -1,5 +0,0 @@
-call C:\bin\midesktop\startAssetInsight.bat
-call C:\bin\midesktop\startBigFix.bat
-call C:\bin\midesktop\startTrend.bat
-call C:\bin\midesktop\startHDX.bat
-call C:\bin\midesktop\startDiskeeper.bat
