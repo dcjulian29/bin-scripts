8350d31de5db10392e4a7ce108964b45c9e0a2d1
Julian Easterling <julian@julianscorner.com>
2012-11-15 13:01:21 -0500

    A python script to sort my VMware VMX files.

---
+++ vmware-vmx-sort.bat
@@ -0,0 +1,20 @@
+@setlocal enableextensions & C:\python\python.exe -x %~f0 %* & goto :EOF
+import sys
+
+filename = sys.argv[1]
+lines = []
+
+fp = open(filename, 'r')
+for line in fp:
+  if not line.strip():
+    continue
+  lines.append(line)
+fp.close()
+
+lines.sort()
+
+fp = open(filename, 'w')
+for line in lines:
+  fp.write(line)
+  fp.flush()
+fp.close()
