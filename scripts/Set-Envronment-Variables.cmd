d89bcbea3af14187866d7d780ad7856d03ce6d12
Julian Easterling <julian@julianscorner.com>
2012-11-15 13:08:34 -0500

    updated my enviornment variable scripts

--- scripts/Set-Envronment-Variables.cmd
+++ scripts/Set-Envronment-Variables.cmd
@@ -1,8 +1,16 @@
-@echo off
+:: Set my enviroment variables
+::------------------------------------------------------------------------------
+GOTO EOF
 
 setx HOME %USERPROFILE%
 
+
 :: Need to be elevated...
+
+setx /m PATH %SYSTEMDRIVE%\bin;%PATH%
+
+:: On development systems
+
 setx /m _NT_SOURCE_PATH SRV*C:\Temp
 setx /m _NT_SYMBOL_PATH SRV*C:\Temp*http://msdl.microsoft.com/download/symbols
 setx /m _NT_SYMCACHE_PATH C:\Temp
