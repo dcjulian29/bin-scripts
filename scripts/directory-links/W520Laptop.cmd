77bae26d5a2e27c608cc88f9f81944d0bbf2cb01
Julian Easterling <julian@julianscorner.com>
2013-02-23 08:49:48 -0500

    Updated my directory links for W520 laptop

--- scripts/directory-links/W520Laptop.cmd
+++ scripts/directory-links/W520Laptop.cmd
@@ -15,8 +15,25 @@ call %SYSTEMDRIVE%\bin\scripts\directory-links\_SetUserProfileDirectory.bat
 ::------------------------------------------------------------------------------
 
 :: Document Folder Links
+mklink /D "%UP%\Documents\AAMVA" D:\AAMVA\docs
 mklink /D "%UP%\Documents\JNet" D:\JNet\docs
 mklink /D "%UP%\Documents\JCoG" D:\JCoG\docs
 mklink /D "%UP%\Documents\Marriott" D:\Marriott\docs
 mklink /D "%UP%\Documents\eBooks" D:\eBooks
+
+:: Picture Folder Links
+mklink /D "%UP%\Picture\AAMVA" D:\AAMVA\pics
+mklink /D "%UP%\Picture\JCoG" D:\JCoG\pics
+mklink /D "%UP%\Picture\JNet" D:\JNet\pics
+
+:: Videos Folder Links
+mklink /D "%UP%\Videos\JNet" D:\JNet\videos
+
+:: Other Links
+mklink /D "%UP%\Desktop\ReviewThis" "%UP%\SkyDrive\Favorites"
+mklink /D "%UP%\Documents\Outlook Files\archives" "D:\archives\Outlook"
+mklink /D "%UP%\Documents\Outlook Files\archives\_ExternalArchive" "L:\archives\Outlook"
+mklink /D "%UP%\Downloads\Magazines\" "%UP%\SkyDrive\Magazines"
+mklink /D "%UP%\Downloads\_D" "D:\Downloads"
+mklink /D "%UP%\Downloads\_TOSOFTWARE" "D:\Downloads\_TOSOFTWARE"
 
