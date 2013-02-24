f0875530a2326fe72630f245182dd422904c64a8
Julian Easterling <julian@julianscorner.com>
2013-02-24 03:18:40 -0500

    Added error checking and the ability to control how many packets are sent to server.

--- iperf-client.bat
+++ iperf-client.bat
@@ -2,6 +2,19 @@
 
 SETLOCAL
 
-C:\bin\network\jperf\bin\iperf.exe -c %* -i 1
+if [%1] == [] goto PROVIDEHOST
 
-popd
+set SEC=15
+if [%2] NEQ [] set SEC=%2
+
+C:\bin\network\jperf\bin\iperf.exe -c %1 -i 1 -t %SEC%
+
+goto EOF
+
+:PROVIDEHOST
+
+echo.
+echo Please provide the name of the host running the server component...
+echo.
+
+:EOF
