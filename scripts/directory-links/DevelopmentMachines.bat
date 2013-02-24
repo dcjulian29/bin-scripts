85484f17caef80f57d1a9c8cadcabc2125871b09
Julian Easterling <julian@julianscorner.com>
2013-02-24 04:20:42 -0500

    Directory Links for development machines

---
+++ scripts/directory-links/DevelopmentMachines.bat
@@ -0,0 +1,23 @@
+@echo off
+
+::------------------------------------------------------------------------------
+call %SYSTEMDRIVE%\bin\scripts\directory-links\_SetUserProfileDirectory.bat
+::------------------------------------------------------------------------------
+
+:: Mecurial
+mklink /J %UP%\.hgrc %SYSTEMDRIVE%\etc\mecurial\hgrc
+
+:: Subversion
+mklink /J %UP%\.subversion %SYSTEMDRIVE%\etc\subversion
+mklink /J %UP%\AppData\Roaming\Subversion %SYSTEMDRIVE%\etc\subversion
+
+
+:: Git
+mklink %UP%\.gitattributes %SYSTEMDRIVE%\etc\git\gitattributes
+mklink %UP%\.gitconfig %SYSTEMDRIVE%\etc\git\gitconfig
+mklink %UP%\.gitignore %SYSTEMDRIVE%\etc\git\gitignore
+mklink %UP%\.gitk %SYSTEMDRIVE%\etc\cygwin\gitk
+mklink %UP%\.Xresources %SYSTEMDRIVE%\etc\cygwin\Xresources
+
+
+
