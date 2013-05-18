6a533c7be4f8e5d5d166dd6871c897c4c6b1034b
Julian Easterling <julian@julianscorner.com>
2013-05-18 16:54:04 -0400

    combined privileged and unprivileged scripts into one and provided a common scipt to call the nmap.exe program

--- nmap-exe.bat
+++ nmap-exe.bat
@@ -1,7 +1,32 @@
-@ECHO OFF
+@echo off
 
-SETLOCAL
+setlocal
 
-SET PATH="C:\bin\network\nmap;%PATH%"
+set PATH=C:\bin\network\nmap;%PATH%
+
+if [%1] == [] goto EXE
+
+echo [%*] | find "--unprivileged" > nul
+If %ERRORLEVEL% EQU 0 goto EXE
+
+call %SYSTEMDRIVE%\bin\_isElevated.cmd YES "%0" %*
+if %ERRORLEVEL% EQU 99 goto PRIVILEGED
+if %ERRORLEVEL% EQU 2 goto UNPRIVILEGED
+
+goto EOF
+
+:UNPRIVILEGED
 
 nmap.exe --unprivileged %*
+
+goto EOF
+
+:PRIVILEGED
+
+:EXE
+
+nmap.exe %*
+
+pause
+
+:EOF
--- nmap.bat
+++ nmap.bat
@@ -1,7 +1,27 @@
-@ECHO OFF
+@echo off
 
-SETLOCAL
+setlocal
 
-SET PATH="C:\bin\network\nmap;%PATH%"
+if [%1] == [] goto EXE
 
-nmap.exe -vv -n -Pn -sT -sV --unprivileged %*
+call %SYSTEMDRIVE%\bin\_isElevated.cmd YES "%0" %*
+if %ERRORLEVEL% EQU 99 goto PRIVILEGED
+if %ERRORLEVEL% EQU 2 goto UNPRIVILEGED
+
+goto EOF
+
+:UNPRIVILEGED
+
+call %SYSTEMDRIVE%\bin\nmap-exe.bat --unprivileged -vv -n -Pn -sT -sV %*
+
+goto EOF
+
+:PRIVILEGED
+
+call %SYSTEMDRIVE%\bin\nmap-exe.bat --privileged -vv -n -Pn -sS -sV -A %*
+goto EOF
+
+:EXE
+call %SYSTEMDRIVE%\bin\nmap-exe.bat 
+
+:EOF
