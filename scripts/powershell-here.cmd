ad9975ec14fb3fd7ee662e436fbe2b197bd62862
Julian Easterling <julian@julianscorner.com>
2013-05-18 16:19:23 -0400

    missed a percent sign in a variable used in script

--- scripts/powershell-here.cmd
+++ scripts/powershell-here.cmd
@@ -8,13 +8,13 @@ echo [HKEY_CLASSES_ROOT\Directory\shell\PowerShellHere] >> %CS%
 echo @="&PowerShell Here" >> %CS%
 echo. >> %CS%
 echo [HKEY_CLASSES_ROOT\Directory\shell\PowerShellHere\command] >> %CS%
-echo @="\"C:\\WINDOWS\\system32\\windowspowershell\\v1.0\\powershell.exe\" -NoExit -ExecutionPolicy unrestricted -Command [Environment]::CurrentDirectory=(Set-Location -LiteralPath:'%L' -PassThru).ProviderPath" >> %CS%
+echo @="\"C:\\WINDOWS\\system32\\windowspowershell\\v1.0\\powershell.exe\" -NoExit -ExecutionPolicy unrestricted -Command [Environment]::CurrentDirectory=(Set-Location -LiteralPath:'%%L' -PassThru).ProviderPath" >> %CS%
 echo. >> %CS%
 echo [HKEY_CLASSES_ROOT\Drive\shell\PowerShellHere] >> %CS%
 echo @="&PowerShell Here" >> %CS%
 echo. >> %CS%
 echo [HKEY_CLASSES_ROOT\Drive\shell\PowerShellHere\command] >> %CS%
-echo @="\"C:\\WINDOWS\\system32\\windowspowershell\\v1.0\\powershell.exe\" -NoExit -ExecutionPolicy unrestricted -Command [Environment]::CurrentDirectory=(Set-Location -LiteralPath:'%L' -PassThru).ProviderPath" >> %CS%
+echo @="\"C:\\WINDOWS\\system32\\windowspowershell\\v1.0\\powershell.exe\" -NoExit -ExecutionPolicy unrestricted -Command [Environment]::CurrentDirectory=(Set-Location -LiteralPath:'%%L' -PassThru).ProviderPath" >> %CS%
 echo. >> %CS%
 
 REG IMPORT %CS%
