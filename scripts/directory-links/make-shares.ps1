a2d9243d30caa2406f4f28d7d55166470abe9a50
Julian Easterling <julian@julianscorner.com>
2013-05-18 16:22:09 -0400

    script to create network shares for guest VMs to use to access files on host system.

---
+++ scripts/directory-links/make-shares.ps1
@@ -0,0 +1,14 @@
+New-Share "C:\Users\Julian\Downloads" Downloads
+New-Share "C:\Users\Julian\Dropbox" Dropbox
+New-Share "C:\Users\Julian\SkyDrive" SkyDrive
+New-Share "C:\Users\Julian\Documents" Documents
+New-Share "C:\Users\Julian\Pictures" Pictures
+New-Share "C:\bin" bin
+New-Share "C:\etc" etc
+New-Share "C:\Users\Julian\Favorites" Favorites
+New-Share "C:\dev" dev
+New-Share "D:\JNet\dev" dev-jnet
+New-Share "D:\JCoG\dev" dev-jog
+New-Share "D:\AAMVA\dev" dev-aamva
+New-Share "K:\software" Software
+
