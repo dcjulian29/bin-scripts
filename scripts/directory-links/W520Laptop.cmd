9374c7ccf9ea2f1f4c62056ad32a208853d38ea7
Julian Easterling <julian@julianscorner.com>
2013-06-22 03:49:36 -0400

    Created a "home" directory to hold all of my "project related folders" and added links to current location of thoses folders. Updated link in user profile to point to home directory. Updated picture and video links with updated folders. updated user profile directory script call.

--- scripts/directory-links/W520Laptop.cmd
+++ scripts/directory-links/W520Laptop.cmd
@@ -11,24 +11,34 @@ goto EOF
 
 
 ::------------------------------------------------------------------------------
-call %SYSTEMDRIVE%\bin\scripts\directory-links\_SetUserProfileDirectory.bat
+call %SYSTEMDRIVE%\bin\scripts\directory-links\_SetUserProfileDirectory.cmd
 ::------------------------------------------------------------------------------
 
+:: "Home" directory Links to Aggregate "Data" Directories
+:: Temporary until next Laptop when only one drive will be in system.
+if not exist %SYSTEMDRIVE%\home mkdir %SYSTEMDRIVE%\home
+mklink /D "%SYSTEMDRIVE%\home\AAMVA" D:\AAMVA
+mklink /D "%SYSTEMDRIVE%\home\JCoG" D:\JCoG
+mklink /D "%SYSTEMDRIVE%\home\JNet" D:\JNet
+mklink /D "%SYSTEMDRIVE%\home\Marriott" D:\Marriott
+
 :: Document Folder Links
-mklink /D "%UP%\Documents\AAMVA" D:\AAMVA\docs
-mklink /D "%UP%\Documents\JNet" D:\JNet\docs
-mklink /D "%UP%\Documents\JCoG" D:\JCoG\docs
-mklink /D "%UP%\Documents\Marriott" D:\Marriott\docs
+mklink /D "%UP%\Documents\AAMVA" %SYSTEMDRIVE%\home\AAMVA\docs
+mklink /D "%UP%\Documents\JNet" %SYSTEMDRIVE%\home\JNet\docs
+mklink /D "%UP%\Documents\JCoG" %SYSTEMDRIVE%\home\JCoG\docs
+mklink /D "%UP%\Documents\Marriott" %SYSTEMDRIVE%\home\Marriott\docs
 mklink /D "%UP%\Documents\eBooks" D:\eBooks
 mklink /D "%UP%\Documents\Virtual Machines" D:\vm
 
 :: Picture Folder Links
-mklink /D "%UP%\Picture\AAMVA" D:\AAMVA\pics
-mklink /D "%UP%\Picture\JCoG" D:\JCoG\pics
-mklink /D "%UP%\Picture\JNet" D:\JNet\pics
+mklink /D "%UP%\Pictures\JCoG" %SYSTEMDRIVE%\home\JCoG\pics
+mklink /D "%UP%\Pictures\JNet" %SYSTEMDRIVE%\home\JNet\pics
+mklink /D "%UP%\Pictures\Other" D:\Pictures
+mklink /D "%UP%\Pictures\Photos" D:\Photos
 
 :: Videos Folder Links
-mklink /D "%UP%\Videos\JNet" D:\JNet\videos
+mklink /D "%UP%\Videos\Other" D:\Videos
+mklink /D "%UP%\Videos\Webcasts" D:\Webcasts
 
 :: Other Links
 mklink /D "%UP%\Desktop\ReviewThis" "%UP%\SkyDrive\Favorites"
@@ -37,4 +47,3 @@ mklink /D "%UP%\Documents\Outlook Files\archives\_ExternalArchive" "L:\archives\
 mklink /D "%UP%\Downloads\Magazines\" "%UP%\SkyDrive\Magazines"
 mklink /D "%UP%\Downloads\_D" "D:\Downloads"
 mklink /D "%UP%\Downloads\_TOSOFTWARE" "D:\Downloads\_TOSOFTWARE"
-
