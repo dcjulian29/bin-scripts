faf098500b3bf2bf31cf495ac566e2a04174446d
Julian Easterling <julian@julianscorner.com>
2013-06-22 03:38:50 -0400

    Not going to have one centralized development folder anymore...

--- scripts/directory-links/dev-links.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-
-mkdir C:\dev
-mklink /D C:\dev\jnet D:\jnet\dev
-mklink /D C:\dev\jcog D:\jcog\dev
-mklink /D C:\dev\aamva D:\AAMVA\dev
-mklink /D C:\dev\marriott D:\Marriott\dev
-mklink /D C:\dev\_ScanForCode D:\Downloads\_ScanForCode
