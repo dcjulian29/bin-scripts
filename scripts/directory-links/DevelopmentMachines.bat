306ccbcf2af0eddc35d070d8ed5f6bbbdc006563
Julian Easterling <julian@julianscorner.com>
2013-06-22 03:42:33 -0400

    removed links for gitattributes and gitk since I'm not using either tool/configuration file and updated user profile script call

--- scripts/directory-links/DevelopmentMachines.bat
+++ scripts/directory-links/DevelopmentMachines.bat
@@ -1,7 +1,7 @@
 @echo off
 
 ::------------------------------------------------------------------------------
-call %SYSTEMDRIVE%\bin\scripts\directory-links\_SetUserProfileDirectory.bat
+call %SYSTEMDRIVE%\bin\scripts\directory-links\_SetUserProfileDirectory.cmd
 ::------------------------------------------------------------------------------
 
 :: Mecurial
@@ -13,7 +13,5 @@ mklink /J %UP%\AppData\Roaming\Subversion %SYSTEMDRIVE%\etc\subversion
 
 
 :: Git
-mklink %UP%\.gitattributes %SYSTEMDRIVE%\etc\git\gitattributes
 mklink %UP%\.gitconfig %SYSTEMDRIVE%\etc\git\gitconfig
 mklink %UP%\.gitignore %SYSTEMDRIVE%\etc\git\gitignore
-mklink %UP%\.gitk %SYSTEMDRIVE%\etc\cygwin\gitk
