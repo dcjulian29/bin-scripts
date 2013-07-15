37924909c9934848fbdd881e28d8ad61d19088bb
Julian Easterling <julian@julianscorner.com>
2013-07-14 23:58:20 -0400

    all of the "shares" were promoted to root level shares.

--- scripts/make-tower-playlist-remote.bat
+++ scripts/make-tower-playlist-remote.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 
-call sed -i 's/D:\\\/\\\\\\\\tower\\\\/g' //nas/shares/public/playlists/*.m3u
+call sed -i 's/D:\\\/\\\\\\\\tower\\\\/g' //nas/public/playlists/*.m3u
 
-start \\nas\shares\public\playlists
+start \\nas\public\playlists
