31ca6b31f612e9bed2075461796aed8680ecfe71
Julian Easterling <julian@julianscorner.com>
2013-09-10 23:20:48 -0400

    Added a link to KDiff settings file.

--- scripts/directory-links/DevelopmentMachines.bat
+++ scripts/directory-links/DevelopmentMachines.bat
@@ -15,3 +15,6 @@ mklink /J %UP%\AppData\Roaming\Subversion %SYSTEMDRIVE%\etc\subversion
 :: Git
 mklink %UP%\.gitconfig %SYSTEMDRIVE%\etc\git\gitconfig
 mklink %UP%\.gitignore %SYSTEMDRIVE%\etc\git\gitignore
+
+:: KDiff3
+mklink %UP%\.kdiff3rc %SYSTEMDRIVE%\etc\kdiff3\kdiff3rc
