ab36ecc159c2204052f79013c7cf5926edc49fd7
Julian Easterling <julian@julianscorner.com>
2013-10-01 16:24:13 -0400

    verify is common regardless of being text or file

---
+++ gpg-verify.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+
+if [%1] == [] goto NOFILE
+
+goto CONT
+
+:NOFILE
+
+echo.
+echo Please provide the file name
+echo.
+
+goto EOF
+
+:CONT
+
+call %~dp0gpg.bat --verify "%1"
+
+endlocal
