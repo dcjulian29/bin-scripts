d16b8f37a104076f348ec384dfc4c9f2901164bb
Julian Easterling <julian@julianscorner.com>
2013-11-02 23:33:14 -0400

    updated these machine specific scripts with latest changes.

--- scripts/directory-links/machine-specific/FEiTower.cmd
+++ scripts/directory-links/machine-specific/FEiTower.cmd
@@ -4,19 +4,18 @@ set TARGETHOME=C:\home
 set ENVIRONMENT=fei
 set DATAHOME=%SYSTEMDRIVE%\home\%ENVIRONMENT%
 
-::------------------------------------------------------------------------------
 call %~dp0_common.cmd
-::------------------------------------------------------------------------------
 
-if not exist %TARGETHOME%\fei\dev mkdir %TARGETHOME%\%ENVIRONMENT%\dev
-if not exist %TARGETHOME%\fei\docs mkdir %TARGETHOME%\%ENVIRONMENT%\docs
-if not exist %TARGETHOME%\fei\pics mkdir %TARGETHOME%\%ENVIRONMENT%\pics
+if not exist %TARGETHOME%\%ENVIRONMENT%\dev mkdir %TARGETHOME%\%ENVIRONMENT%\dev
+if not exist %TARGETHOME%\%ENVIRONMENT%\docs mkdir %TARGETHOME%\%ENVIRONMENT%\docs
+if not exist %TARGETHOME%\%ENVIRONMENT%\pics mkdir %TARGETHOME%\%ENVIRONMENT%\pics
 
-:: Folder Links
 mklink /D "%SYSTEMDRIVE%\dev" %DATAHOME%\dev
 mklink /D "%UP%\Documents\FEi" %DATAHOME%\docs
 mklink /D "%UP%\Pictures\FEi" %DATAHOME%\pics
 
-
-:: Virtual Machines
 if not exist %SYSTEMDRIVE%\vm mkdir %SYSTEMDRIVE%\vm
+mklink /D "%UP%\VirtualBox VMs" %SYSTEMDRIVE%\vm
+
+call %~dp0\..\AllMachines.bat
+call %~dp0\..\DevelopmentMachines.bat
--- scripts/directory-links/machine-specific/TOWER.cmd
+++ scripts/directory-links/machine-specific/TOWER.cmd
@@ -1,26 +1,17 @@
 @echo off
 
-echo.
-echo Don't run this script directly...
-echo.
-echo 1. Log in with an alternative account (elevate?)
-echo 2. Open the script in Notepad
-echo 3. Run commands in an elevated shell as needed...
-echo.
-goto EOF
+set TARGETHOME=D:\JNet
+set ENVIRONMENT=jnet
+set DATAHOME=%SYSTEMDRIVE%\home\%ENVIRONMENT%
 
+call %~dp0_common.cmd
 
-::------------------------------------------------------------------------------
-call %SYSTEMDRIVE%\bin\scripts\directory-links\_SetUserProfileDirectory.cmd
-::------------------------------------------------------------------------------
-
-:: "Home" directory Links to Aggregate "Data" Directories
-:: Temporary until next Laptop when only one drive will be in system.
-if not exist %SYSTEMDRIVE%\home mkdir %SYSTEMDRIVE%\home
 mklink /D "%SYSTEMDRIVE%\home\FEi" D:\FEi
 mklink /D "%SYSTEMDRIVE%\home\SNVUSA" D:\SNVUSA
 mklink /D "%SYSTEMDRIVE%\home\JCoG" D:\JCoG
-mklink /D "%SYSTEMDRIVE%\home\JNet" D:\JNet
+
+if not exist "E:\Virtual Machines" mkdir "E:\Virtual Machines"
+mklink /D %SYSTEMDRIVE%\vm "E:\Virtual Machines"
 
 :: Document Folder Links
 mklink /D "%UP%\Documents\FEi" %SYSTEMDRIVE%\home\FEi\docs
@@ -28,23 +19,16 @@ mklink /D "%UP%\Documents\JNet" %SYSTEMDRIVE%\home\JNet\docs
 mklink /D "%UP%\Documents\JCoG" %SYSTEMDRIVE%\home\JCoG\docs
 mklink /D "%UP%\Documents\eBooks" %SYSTEMDRIVE%\home\jnet\ebooks
 mklink /D "%UP%\Documents\PaperPort" %SYSTEMDRIVE%\home\jnet\paperport
-mklink /D "%UP%\Documents\Virtual Machines" "E:\Virtual Machines"
+mklink /D "%UP%\Documents\Virtual Machines" %SYSTEMDRIVE%\vm
 
 :: Picture Folder Links
 mklink /D "%UP%\Pictures\FEi" %SYSTEMDRIVE%\home\FEi\pics
 mklink /D "%UP%\Pictures\JCoG" %SYSTEMDRIVE%\home\JCoG\pics
 mklink /D "%UP%\Pictures\JNet" %SYSTEMDRIVE%\home\JNet\pics
 
-:SkyDrive
+:: Other Links
 if not exist %UP%\SkyDrive mkdir %UP%\SkyDrive
 mklink /D "%UP%\Documents\SkyDrive" "%UP%\SkyDrive"
 
-
-mklink /D %SYSTEMDRIVE%\bin %SYSTEMDRIVE%\home\jnet\bin
-mklink /D %SYSTEMDRIVE%\etc %SYSTEMDRIVE%\home\jnet\etc
-
-:: Virtual Machines
-if not exist E:\vm mkdir E:\vm
-mklink /D %SYSTEMDRIVE%\vm "E:\Virtual Machines"
-
-:: Now Run "All Machines"
+call %~dp0\..\AllMachines.bat
+call %~dp0\..\DevelopmentMachines.bat
