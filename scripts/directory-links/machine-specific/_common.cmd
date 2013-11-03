e0252f1bd3f6a6829a1fb230923652f76c855162
Julian Easterling <julian@julianscorner.com>
2013-11-02 23:34:58 -0400

    Added short-cut for BTSync to all machines.

--- scripts/directory-links/machine-specific/_common.cmd
+++ scripts/directory-links/machine-specific/_common.cmd
@@ -1,16 +1,7 @@
-echo.
-echo Don't run this script directly...
-echo.
-echo 1. Log in with an alternative account (elevate?)
-echo 2. Open the script in Notepad
-echo 3. Run commands in an elevated shell as needed...
-echo.
-pause
-exit /b 1
-
-::------------------------------------------------------------------------------
 call %~dp0..\_SetUserProfileDirectory.cmd
-::------------------------------------------------------------------------------
+
+echo Home Directory: %TARGETHOME%
+echo.
 
 :: "Home" directory Links to Aggregate "Data" Directories
 if not exist %SYSTEMDRIVE%\home mkdir %SYSTEMDRIVE%\home
@@ -27,3 +18,5 @@ mklink /D %SYSTEMDRIVE%\bin %DATAHOME%\bin
 if not exist %DATAHOME%\etc mkdir %DATAHOME%\etc
 mklink /D %SYSTEMDRIVE%\etc %DATAHOME%\etc
 
+if not exist %UP%\BTSync mkdir %UP%\BTSync
+mklink /D "%UP%\Documents\BTSync" "%UP%\BTSync"
