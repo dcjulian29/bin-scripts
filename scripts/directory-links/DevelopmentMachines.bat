66405f8cd58a2bbf60ffe6ad666f3e38aa2d67df
Julian Easterling <julian@julianscorner.com>
2013-11-26 17:59:03 -0500

    Removed "machine customization" scripts since they are typically only run once during a machine setup.

--- scripts/directory-links/DevelopmentMachines.bat
+++
@@ -1,9 +0,0 @@
-:: Make sure to run the Machine-Specific Directory Script First...
-::-----------------------------------------------------------------
-
-:: Git
-mklink %UP%\.gitconfig %SYSTEMDRIVE%\etc\git\gitconfig
-mklink %UP%\.gitignore %SYSTEMDRIVE%\etc\git\gitignore
-
-:: KDiff3
-mklink %UP%\.kdiff3rc %SYSTEMDRIVE%\etc\kdiff3\kdiff3rc
