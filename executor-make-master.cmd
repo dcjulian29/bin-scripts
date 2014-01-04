6fda3dd10500f1939d2dd5e28305a37ce56c23d8
Julian Easterling <julian@julianscorner.com>
2014-01-04 16:40:02 -0500

    Added scripts that allow me to "copy" my executor files to a temporary directory and then to make a "master" copy of the shortcut list back to the "persistant" location

---
+++ executor-make-master.cmd
@@ -0,0 +1,41 @@
+@echo off
+
+if [%COMPUTERNAME%] == [TOWER] goto MAKEMASTER
+if [%COMPUTERNAME%] == [LAPTOP] goto MAKEMASTER
+if [%COMPUTERNAME%] == [JULIAN] goto MAKEMASTER
+
+echo.
+echo You can't run this command on this computer.
+
+goto EOF
+
+:MAKEMASTER
+
+set EXECUTOR=%TEMP%\executor
+
+set EXDIR=%SYSTEMDRIVE%\etc\executor
+set BVER=%date:~-4,4%%date:~-10,2%%date:~-7,2%
+set BVER=%BVER%_%time:~0,2%%time:~3,2%%time:~6,2%
+set BVER=%BVER: =0%
+
+echo Backing up original executor.ini file...
+copy %EXDIR%\executor.ini %EXDIR%\backup\%BVER%.ini
+
+echo.
+echo Copying new master executor.ini file...
+for /f "usebackq delims=" %%O in (`dir /b /ad %SYSTEMDRIVE%\home`) do (
+    for /f "usebackq delims=" %%D in (`dir /b /ad %SYSTEMDRIVE%\home\%%O`) do (
+        if [%%D] EQU [etc] (
+            if exist "%SYSTEMDRIVE%\home\%%O\%%D\executor" (
+                echo %EXECUTOR%\executor.ini --^> %SYSTEMDRIVE%\home\%%O\%%D\executor\executor.ini
+                copy /V %EXECUTOR%\executor.ini %SYSTEMDRIVE%\home\%%O\%%D\executor\executor.ini
+            )
+        )
+    )
+)
+
+
+:EOF
+
+echo.
+pause
---
+++ executor-run.cmd
@@ -0,0 +1,53 @@
+@echo off
+
+setlocal
+
+set EXSRC=%SYSTEMDRIVE%\Tools\apps\executor
+set EXETC=%SYSTEMDRIVE%\etc\executor
+set EXDST=%TEMP%\executor
+
+if exist %EXDST% (
+  echo Stopping the running Executor process...
+  %EXDST%\executor.exe -exit
+  call %SYSTEMDRIVE%\Tools\binaries\sleep.bat 5
+  echo Removing existing temporary files...
+  rmdir /s /q %EXDST% >nul
+  if exist %EXDST% (
+    echo A process has a lock on the directory, please remove it manually.
+    pause
+    :: One more try...
+    rmdir /s /q %EXDST% >nul
+  )
+  if exist %EXDST% exit 1
+)
+
+mkdir %EXDST%
+
+copy %EXSRC%\executor.exe %EXDST% >nul
+copy %EXETC%\executor.ini %EXDST% >nul
+copy %EXSRC%\defaulticon.ico %EXDST% >nul
+copy %EXSRC%\hookwinr.dll %EXDST% >nul
+copy %EXETC%\*.wav %EXDST% >nul
+
+mkdir %EXDST%\skins
+copy %EXSRC%\skins\*.* %EXDST%\skins >nul
+
+pushd %EXDST%
+
+:: "touch" all of the files so that they do not appear to be "older than 7 days" when the
+:: dir-clean-temp script runs...
+
+for /R "." %%G in (.) do (
+  pushd %%G
+  for /f "delims=|" %%F in ('dir /B /A:-D .') do (
+    copy "%%F" +,, >nul
+  )
+  popd
+)
+
+echo Starting Executor
+start Executor.exe -s
+
+popd
+
+endlocal
