e4b74f0962dbaeb1621ef8a7ea5a1f69b85d6992
Julian Easterling <julian@julianscorner.com>
2014-01-23 15:18:51 -0500

    removed VMware scripts... after 6 months of using Virtual Box, I think it is safe to remove these.

--- vmware/_vm_settings.cmd
+++
@@ -1,4 +0,0 @@
-set VMDIR=%USERPROFILE%\Documents\Virtual Machines
-set VMPROG=%PF32%\VMware\VMware Player
-set VPLAYER=%VMPROG%\vmplayer.exe
-set VDM=%SYSTEMDRIVE%\bin\vmware\vmware-vdiskmanager.exe
