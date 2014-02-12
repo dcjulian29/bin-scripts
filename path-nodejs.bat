a7089b6e09d10090c6aeb79b589aa09efe77e649
Julian Easterling <julian@julianscorner.com>
2014-02-11 22:04:05 -0500

    Added NPM directory to path.

--- path-nodejs.bat
+++ path-nodejs.bat
@@ -2,3 +2,4 @@
 
 if exist "%ProgramFiles%\nodejs" path %PATH%;%ProgramFiles%\nodejs
 if exist "%ProgramW6432%\nodejs" path %PATH%;%ProgramW6432%\nodejs
+if exist %APPDATA%\npm path %PATH%;%APPDATA%\npm
