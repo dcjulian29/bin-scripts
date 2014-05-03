603a194727e3863384b31a90efa5ba4280a9c3d6
Julian Easterling <julian@julianscorner.com>
2014-05-03 15:41:32 -0400

    fixed the directory that the shutdown script was looking for when checking for elevation.

--- psshutdown.bat
+++ psshutdown.bat
@@ -1,6 +1,6 @@
 @echo off
 
-call %SYSTEMDRIVE%\bin\_isElevated.cmd YES "%0" %*
+call %SYSTEMDRIVE%\Tools\binaries\_isElevated.cmd YES "%0" %*
 if %ERRORLEVEL% NEQ 99 goto EOF
 
 @%SYSTEMDRIVE%\Tools\apps\sysinternals\psshutdown.exe %*
