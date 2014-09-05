de3c88a20290191d04acd950c79f7eeb3f0b9891
Julian Easterling <julian@julianscorner.com>
2014-09-05 17:51:28 -0400

    Make sure that the destination folder for webcast downloads exists before downloading file....

--- webcast-download.bat
+++ webcast-download.bat
@@ -6,13 +6,15 @@ if [%1] == [] goto PROVIDESRC
 
 set SRC=%1
 
-set DST=%USERPROFILE%\Videos\
+set DST=%USERPROFILE%\Downloads\webcasts\
 
 if [%2] == [] goto STARTDOWNLOAD
 
 set DST=%2
 
 :STARTDOWNLOAD
+
+if not exist %DST% mkdir %DST%
 
 call %~dp0wget.bat "%SRC%" -P "%DST%" --continue --tries=10 --restrict-file-names=windows
 
