e918acadc23c26112563cc50696e9a375991a54a
Julian Easterling <julian@julianscorner.com>
2016-11-04 14:47:51 -0400

    This function has been replaced with a PowerShell Function

--- path-reset.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set PATH=%WINDIR%;%WINDIR%\system32;%WINDIR%\System32\Wbem;%WINDIR%\System32\WindowsPowerShell\v1.0;%ALLUSERSPROFILE%\chocolatey\bin
-
-if exist "%SYSTEMDRIVE%\tools\binaries" set PATH=%SYSTEMDRIVE%\tools\binaries;%PATH%
-
-setx /m PATH %PATH%
-
-reg delete HKCU\Environment /v Path /f
